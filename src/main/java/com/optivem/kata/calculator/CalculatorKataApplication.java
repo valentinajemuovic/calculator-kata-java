94c29af1c30dfe1a570692db31785938951fddfe
Valentina Cupać <valentina.cupac@optivem.com>
2022-04-29 08:57:25 +0200

    Project Setup

---
+++ src/main/java/com/optivem/kata/calculator/CalculatorKataApplication.java
@@ -0,0 +1,13 @@
+package com.optivem.kata.calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorKataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorKataApplication.class, args);
+	}
+
+}
